6a639a3bd87f55fdf90afaab0c0df7ffd07319c0
hidtiger <xelmari@gmail.com>
2012-04-16 17:24:25 +0900

    commit

--- s.rb
+++ s.rb
@@ -1,8 +1,25 @@
+
 require 'sinatra'
+require 'nokogiri'
+require 'open-uri'
 
 
 get '/' do
-	puts request.query_string
-	"hogehoge"
+#	puts request.query_string
+
+	uri = 'http://juggly.cn/feed'
+	xpath = '/html/body/div/div[3]/div[2]'
+
+	rss = Nokogiri::XML open uri
+	rss.search('item').each do |v|
+		item = Nokogiri open v.at('link').text
+		contents = item.at xpath
+		contents.search('script').each do |v| v.remove end
+		contents.search('comment()').each do |v| v.remove end
+		v.at('description').child.remove
+		v.at('description') << Nokogiri::XML::CDATA.new(v.document, contents.to_s)
+		sleep 1
+	end
+	rss.to_s
 end
 
